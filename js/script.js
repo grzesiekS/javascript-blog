7228cd60734580fa9a09f4328050ecb2f38484ac
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-08 14:19:48 +0200

    Add js script for adding class 'active' to link in titleClickHandler

--- js/script.js
+++ js/script.js
@@ -5,25 +5,33 @@
     console.log('links: ', links);
 });*/
     const titleClickHandler = function() {
+        const clickedElement = this;
+
         console.log('Link was clicked!');
         console.log(event);
 
-        /* remove class 'active' from all article links  */
+        /* [DONE] remove class 'active' from all article links  */
+
         const activeLinks = document.querySelectorAll('.titles a.active');
         
         for(let activeLink of activeLinks) {
             activeLink.classList.remove('active');
         }
 
-        /* add class 'active' to the clicked link */
+        /* [DONE] add class 'active' to the clicked link */
 
-        /* remove class 'active' from all articles */
+        console.log('clickedElement:', clickedElement);
+
+        clickedElement.classList.add('active');
+
+        /* [DONE] remove class 'active' from all articles */
+
         const activeArticles = document.querySelectorAll('.post.active');
 
         for(let activeArticle of activeArticles) {
             activeArticle.classList.remove('active');
         }
-        
+
         /* get 'href' attribute from the clicked link */
 
         /* find the correct article using the selector (value of 'href' attribute) */
