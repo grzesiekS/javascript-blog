de2f6edb419b0e212baf06eaabee071d53955926
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-09 13:50:53 +0200

    Add function generateTitleLinks and const varaibles

--- js/script.js
+++ js/script.js
@@ -49,6 +49,30 @@
         targetArticle.classList.add('active');
     }
 
+    const optArticleSelector = '.post',
+        optTitleSelector = '.post-title',
+        optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+
+    /* remove contents of titleList */
+
+    /* for each article */
+
+        /* get the article id */
+
+        /* find the title element */
+
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        /* insert link into titleList */
+
+    }
+
+    generateTitleLinks();
+
     const links = document.querySelectorAll('.titles a');
 
     for(let link of links){
